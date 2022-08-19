462222abcf4634b48f6c87e190bd10d866724e43
donaldmema <mema.donald@gmail.com>
2022-08-19 14:10:22 +0200

    Add set piece function

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -6,8 +6,8 @@ export function Table({ body }: Props) {
   return (
     <table style={{ width: 650, height: 500 }}>
       <tbody>
-        {body.map((row) => (
-          <TableRow row={row} board= {body}/>
+        {body.map((row, rowIndex) => (
+          <TableRow key={rowIndex} row={row} rowIndex={rowIndex} board={body} />
         ))}
       </tbody>
     </table>
--- src/components/TableRow.tsx
+++ src/components/TableRow.tsx
@@ -1,21 +1,23 @@
 import { useState } from "react";
 
-type Props = { row: number[]; board: number[][] };
+type Props = { row: number[]; rowIndex: number; board: number[][] };
 
-export function TableRow({ row, board }: Props) {
+export function TableRow({ row, rowIndex, board }: Props) {
   let gamer1 = 1;
   let gamer2 = 2;
-  const [currentPlayer, setCurrentPlayer] = useState(gamer1);
+  const [currentPlayer, setCurrentPlayer] = useState(0);
 
-  function setPiece() {
-    let coordinates = this.id.split("-");
+  function setPiece(target) {
+    let coordinates = target.id.split("-");
     let x = parseInt(coordinates[0]);
     let y = parseInt(coordinates[1]);
+    console.log(x, y);
 
     board[x][y] = currentPlayer;
+    console.log(board);
 
     if (currentPlayer === gamer1) {
-      this.className("tile gamer1");
+      target.classList.add("tile gamer1");
       setCurrentPlayer(gamer2);
     }
   }
@@ -24,9 +26,13 @@ export function TableRow({ row, board }: Props) {
       {row.map((cell, cellIndex) => (
         <td key={cellIndex}>
           <button
-            className="tile"
-            id={`${cell}-${cellIndex}`}
-            onClick={() => setPiece}
+            className={
+              "tile" +
+              (currentPlayer === 1 ? " gamer1" : "") +
+              (currentPlayer === 2 ? " gamer2" : "")
+            }
+            id={`${rowIndex}-${cellIndex}`}
+            onClick={(event) => setPiece(event.target)}
           ></button>
         </td>
       ))}
